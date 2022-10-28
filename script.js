05235e8e8e38d8c648ff2e4879a981cf0868c2e6
Odil-web <116338011+odik5222@users.noreply.github.com>
2022-10-28 10:34:54 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+ let clientName = prompt('What is ur name?')
+if (clientName === 'Alex ') {
+  console.log('Welcome')
+} else {
+  console.log('Пользователь не найден,досвидули')
+}
+
+let number = prompt('What is ur cart number?')
+if (number === '7777'){
+}
+  let balance = prompt ('cash out ')
+if (balance === '10000') 
+console.log('Все отлично')
+ else{
+  console.log('EROR 404')
+} 
+
+
+
+
+/* let name = 'alex'
+let money = +10000
+let account = 7777
+let userName = prompt('What is your name?')
+let userPWD = prompt('Account number')
+let userMoney = +prompt('Сash out')
+let m = '-'
+
+
+
+if(userName === name && userPWD === account){
+    console.log(name + money);
+} else if(userName != name || userPWD != account){
+    console.log('Досвидули');
+} else if(m === '-' && userMoney <= 10000){
+    console.log(money - userMoney, userMoney);
+}else {  
+    console.log('Недостаточно средств');
+} */
